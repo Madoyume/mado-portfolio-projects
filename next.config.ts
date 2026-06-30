cde7719bd1336fe10f52790246d2026b8803a240
てぃ <166586496+Madoyume@users.noreply.github.com>
2026-06-30 15:50:16 +0900

    feat: outputFileTracingIncludesを追加し、@libsqlパッケージのトレースを含める

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["cloudinary"],
+  outputFileTracingIncludes: {
+    "/*": ["node_modules/@libsql/**/*"],
+  },
 };
 
 export default nextConfig;
